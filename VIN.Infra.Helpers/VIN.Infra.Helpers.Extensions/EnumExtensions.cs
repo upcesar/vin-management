61fea7fdceb8fa703b80e1458f19bd5cf8ae3f09
César M. Urdaneta <upcesar@gmail.com>
2020-06-02 22:11:06 -0300

    Added Extension Helpers

---
+++ VIN.Infra.Helpers/VIN.Infra.Helpers.Extensions/EnumExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VIN.Helpers.Extensions
+{
+    /// <summary>
+    /// Helper for Enumerators
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Convert a string to its respective Enumerator.
+        /// </summary>
+        /// <param name="stringValue">String value to find and get the Enum</param>
+        /// <returns>Enum value for the string or default</returns>
+        public static T? ToEnum<T>(this string stringValue) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum) throw new ArgumentException("T must be an enumerated type");
+
+            if (Enum.TryParse(stringValue?.ToUpper(), out T enumeratorValue) || Enum.TryParse(stringValue?.ToLower(), out enumeratorValue))
+                return enumeratorValue;
+
+            return default;
+        }
+
+        /// <summary>
+        /// Convert a string to its respective Enumerator.
+        /// </summary>
+        /// <param name="stringValue">String value to find and get the Enum</param>
+        /// <param name="defaultValue">Optional - Set default value when not found</param>
+        /// <returns>Enum value for the string or default</returns>
+        public static T ToEnum<T>(this string stringValue, T defaultValue) where T : struct, IConvertible
+            => ToEnum<T>(stringValue) ?? defaultValue;
+    }
+}
---
+++ VIN.Infra.Helpers/VIN.Infra.Helpers.Extensions/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace VIN.Infra.Helpers.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Remove special characters (Diacritics) in a string
+        /// </summary>
+        /// <param name="text">string to be Removed the Diacritics characters</param>
+        /// <returns>String without Diacritics</returns>
+        public static string RemoveDiacritics(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return text;
+
+            text = text.Normalize(NormalizationForm.FormD);
+            
+            var chars = text.Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                            .ToArray();
+
+            return new string(chars).Normalize(NormalizationForm.FormC);
+        }
+    }
+}
